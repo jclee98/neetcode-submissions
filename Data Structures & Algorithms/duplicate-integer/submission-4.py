85774ed10e10e4b85f6976cde1f5d3171705c168
jclee98 <72079284+jclee98@users.noreply.github.com>
2026-08-30 22:38:37 +0800

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,49 @@
+"""
+0. Intuoition
+
+1. What do tjhey wany?
+- Bool 
+- Do what
+..
+
+2. edge cases 
+
+3. naive
+
+4. Pattern 
+
+5. Complexities 
+
+"""
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for i in nums:
+            if i not in hashset:
+                hashset.add(i)
+            else:
+                return True
+        return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #     hashset = set()
+    #     for n in nums:
+    #         if n not in hashset:
+    #             hashset.add(n)
+    #         else:
+    #             return True
+    #     return False
+        
