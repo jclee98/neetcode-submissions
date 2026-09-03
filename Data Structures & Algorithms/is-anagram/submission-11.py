7a5944325b8c6926f4229aff54fbb81abe492cd3
jclee98 <72079284+jclee98@users.noreply.github.com>
2026-09-03 22:26:45 +0800

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,84 @@
+'''
+check if length is the same
+
+create hashmap for eaech string to store each char's count
+
+populate hashmap by iterating the string and adding 1, use .get(x, 0)
+
+for each val in first compare with the val in the other one
+
+OR
+
+just simply return counter(s) == counter(t)
+'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hashS, hashT = {}, {}
+        for i in range(len(s)):
+            hashS[s[i]] = 1 + hashS.get(s[i], 0)
+            hashT[t[i]] = 1 + hashT.get(t[i], 0)
+        for key in hashT:
+            if hashT[key] != hashS.get(key, 0):
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        hashmapS, hashmapT = {}, {}
+        for i in range(len(s)):
+            hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0)
+            hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0)
+        for i in hashmapS:
+            if hashmapS[i] != hashmapT.get(i, 0):
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if len(s) != len(t):
+            return False
+        hashS, hashT = {}, {}
+        for i in range(len(s)):
+            hashS[s[i]] = hashS.get(s[i], 0) + 1
+            hashT[t[i]] = hashT.get(t[i], 0) + 1
+        for i in hashS:
+            if hashS[i] != hashT.get(i,0):
+                return False
+        return True
+        
