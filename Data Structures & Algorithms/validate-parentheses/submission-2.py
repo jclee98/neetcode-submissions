0286b7173b5980ac648d2436cf84a860f1f8fe63
jclee98 <72079284+jclee98@users.noreply.github.com>
2026-09-08 23:08:41 +0800

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,131 @@
+'''
+Create a dict to store the closing bracket to its opening bracket
+
+Create a stack
+
+Iterate the brackets 
+    if it is a open, add to the stack. 
+    Else, 
+        if stack is empty, return false.
+        else, check if the opening popped value matches the close in the dict, false if it not
+lastly, check if the stack is empty, by returning not stack
+    
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dictCT = {')':'(', ']':'[', '}':'{'}
+        stack = []
+        for i in s:
+            if i not in dictCT:
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                else:
+                    popped = stack.pop()
+                    if popped != dictCT[i]:
+                        return False
+        return not stack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        stack = []
+        dictCT = {')':'(', '}':'{', ']':'['}
+        for i in s:
+            if i not in dictCT:
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                else:
+                    popped = stack.pop()
+                    if popped != dictCT[i]:
+                        return False
+        return not stack
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # stack = []
+        # closeToOpen = {')': '(', ']':'[', '}': '{'}
+        # for bracket in s:
+        #     if bracket not in closeToOpen:
+        #         stack.append(bracket)
+        #     else:
+        #         if not stack:
+        #             return False
+        #         else:
+        #             popped = stack.pop()
+        #             if popped != closeToOpen[bracket]:
+        #                 return False
+        # return not stack
+        
