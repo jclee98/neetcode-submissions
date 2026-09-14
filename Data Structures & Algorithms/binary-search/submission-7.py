0e47a60949cca2c7d702cbf7d01c875f55084532
jclee98 <72079284+jclee98@users.noreply.github.com>
2026-09-14 22:58:03 +0800

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.py
@@ -0,0 +1,22 @@
+'''
+Two pointer method, L and R
+Get the mid point
+While L <= R
+    if target > value at mid, replace L with mid + 1
+    if target < value at mid, replace R with mid - 1
+    if value at mid point equals to the target, return mid
+return -1
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        L,R = 0, len(nums) - 1
+        while L<=R:
+            mid = (L+R)//2
+            if target > nums[mid]:
+                L = mid + 1
+            if target < nums[mid]:
+                R = mid - 1
+            if target == nums[mid]:
+                return mid
+        return -1
+            
